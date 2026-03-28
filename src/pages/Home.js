0179e94e6c9e698c83f8b6aeb8c73a0a1445dcf3
Danilo <danilo@rpinfo.com.br>
2026-03-28 17:20:56 -0300

    teste de CI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ function Home() {
     <div style={{ padding: "20px" }}>
       <h1>Gerenciador de Cadastro de Produtos</h1>
       <p>Projeto piloto simples usando React, para a disciplina de DevOps</p>
+
+      <p>Iniciado teste de CI</p>
     </div>
   );
 }
